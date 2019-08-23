61b9fec98e8d58cd8d7e93859f0474debf034421
ahmadtc1 <ahmadtc2000@gmail.com>
2019-08-23 12:51:17 -0400

    Restructuring loops to fit all image sizes

--- gaussianBlur.py
+++ gaussianBlur.py
@@ -14,7 +14,7 @@ print(img_array)
 print(img_array.shape)
 
 
-kernel = np.array([[1,1,1],[1,1,1],[1,1,1]])
+kernel = np.array([[1,2,1],[2,4,2],[1,2,1]])
 kernelOne = kernel[0][0]
 kernelTwo = kernel[1][0]
 kernelThree = kernel[2][0]
@@ -31,12 +31,14 @@ blurred_img_array = img_array
 division_factor = 14
 sum = 0
 
-for x in range(2,1598, 3):
-    for y in range(2,2398, 5):
+for x in range(1, img_array.shape[0] - 1):
+    for y in range(1, img_array.shape[1] - 1):
+        sum = 0
         sum += img_array[x-1][y-1] * kernelOne
         sum += img_array[x][y-1] * kernelTwo
         sum += img_array[x+1][y-1] * kernelThree
         sum += img_array[x-1][y] * kernelFour
+        sum += img_array[x][y] * kernelFive
         sum += img_array[x+1][y] * kernelSix
         sum += img_array[x-1][y+1] * kernelSeven
         sum += img_array[x][y+1] * kernelEight
@@ -45,5 +47,5 @@ for x in range(2,1598, 3):
         average = sum / division_factor
         blurred_img_array[x][y] = average
 
-plt.imshow(blurred_img_array)
+plt.imshow(blurred_img_array, cmap='gray')
 plt.show()
