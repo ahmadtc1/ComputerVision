7fab322f00b38dc7d79c1d6a56c81c69ac209528
ahmadtc1 <ahmadtc2000@gmail.com>
2019-08-25 01:56:28 -0400

    Creating a script file for easy edge detection of all images within a folder

---
+++ edgeDetectInFolder.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import argparse
+import glob
+
+#Set up an argument parser to have the user input the desired folder from which they would like to edge detect images when executing the script
+ap = argparse.ArgumentParser()
+ap.add_argument("--i", "--images", required = True,
+    help = "Path to set of input images"
+)
+args = vars(ap.parse_args())
+
+for imagePath in glob.glob(args["i"] + "/*.jpg"):
+    try:
+        #Load the image as grayscale into an array
+        img_array = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+        #Gaussian blur the image to remove high noise
+        blurred = cv2.GaussianBlur(img_array, (3, 3), 0)
+
+        #Calculate upper and lower threshold values for the image using a statistical formula
+        v = np.median(blurred)
+        sigma = 0.33
+        lower = int(max(0, (1.0 - sigma) * v))
+        upper = int(min(255, (1.0 + sigma) * v))
+
+        #Perform edge detection on the image and display it
+        edges = cv2.Canny(blurred, lower, upper)
+        plt.imshow(edges, cmap='gray')
+        plt.show()
+
+    except Exception as e:
+        pass
+
+print("Done edge detecting all compatible images")
