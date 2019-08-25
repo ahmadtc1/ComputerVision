25ad04cb2f13e0786ff0eff7c5aa589fa93d801b
ahmadtc1 <ahmadtc2000@gmail.com>
2019-08-25 00:40:27 -0400

    Calculate thresholds for canny detection and then perform canny detection

--- edgeDetection.py
+++ edgeDetection.py
@@ -2,3 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
+
+#Set the directory for the desired photo
+FILE_DIRECTORY = "/Users/AhmadTC/Desktop/Pictures:Vids/cube-01-01-01.jpg"
+FILE_DIRECTORY_GENERAL = "/Users/AhmadTC/Desktop/Pictures:Vids"
+
+#Load the image as a grayscale image into an array
+img_array = cv2.imread(FILE_DIRECTORY, cv2.IMREAD_GRAYSCALE)
+
+plt.imshow(img_array, cmap='gray')
+plt.show()
+sum = 0
+total = img_array.shape[0] * img_array.shape[1]
+
+for x in range(img_array.shape[0]):
+    for y in range(img_array.shape[1]):
+        sum += img_array[x][y]
+
+
+mean = sum / total
+
+#perform Canny edge detection using openCV
+edges = cv2.Canny(img_array, mean*0.75, mean*1.25)
+
+#Display the edge detected image
+plt.imshow(edges, cmap='gray')
+plt.show()
