68f4b5e43f527a9a11a44f455109872a1a25db56
ahmadtc1 <ahmadtc2000@gmail.com>
2019-08-25 01:03:28 -0400

    Applying gaussian blur to smooth image before edge detection

--- edgeDetection.py
+++ edgeDetection.py
@@ -5,22 +5,31 @@ import cv2
 from skimage import filters
 
 #Set the directory for the desired photo
-FILE_DIRECTORY = "/Users/AhmadTC/Desktop/Pictures:Vids/cube-01-01-01.jpg"
+FILE_DIRECTORY = "/Users/AhmadTC/Desktop/Pictures:Vids/bridge.jpg"
 FILE_DIRECTORY_GENERAL = "/Users/AhmadTC/Desktop/Pictures:Vids"
 
-#Load the image as a grayscale image into an array
+#Load the image as grayscale into an array
 img_array = cv2.imread(FILE_DIRECTORY, cv2.IMREAD_GRAYSCALE)
 
 #Show the image before performing edge detection
 plt.imshow(img_array, cmap='gray')
 plt.show()
 
-#Obtain the otsu threshold to be used in canny edge detection
-otsu = filters.threshold_otsu(img_array)
+#Apply a gaussian blur to remove high frequency noise
+blurred = cv2.GaussianBlur(img_array, (3, 3), 0)
+
+plt.imshow(blurred, cmap='gray')
+plt.show()
 
 
-#perform Canny edge detection using the otsu threshold
-edges = cv2.Canny(img_array, otsu*0.5, otsu)
+#Calculating the upper and lower threshold values for Canny edge detection
+v = np.median(blurred)
+sigma = 0.33
+lower = int(max(0, (1.0 - sigma) * v))
+upper = int(min(255, (1.0 + sigma) * v))
+
+#perform Canny edge detection using the calculated threshold values
+edges = cv2.Canny(blurred, lower, upper)
 
 #Display the edge detected image
 plt.imshow(edges, cmap='gray')
