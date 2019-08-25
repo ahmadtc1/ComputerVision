b651d2583c85bd65abfdf008a41699c242a7928e
ahmadtc1 <ahmadtc2000@gmail.com>
2019-08-25 00:46:12 -0400

    Calculating max and min thresholds using otsu threshold

--- edgeDetection.py
+++ edgeDetection.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
+from skimage import filters
 
 #Set the directory for the desired photo
 FILE_DIRECTORY = "/Users/AhmadTC/Desktop/Pictures:Vids/cube-01-01-01.jpg"
@@ -10,20 +11,16 @@ FILE_DIRECTORY_GENERAL = "/Users/AhmadTC/Desktop/Pictures:Vids"
 #Load the image as a grayscale image into an array
 img_array = cv2.imread(FILE_DIRECTORY, cv2.IMREAD_GRAYSCALE)
 
+#Show the image before performing edge detection
 plt.imshow(img_array, cmap='gray')
 plt.show()
-sum = 0
-total = img_array.shape[0] * img_array.shape[1]
 
-for x in range(img_array.shape[0]):
-    for y in range(img_array.shape[1]):
-        sum += img_array[x][y]
+#Obtain the otsu threshold to be used in canny edge detection
+otsu = filters.threshold_otsu(img_array)
 
 
-mean = sum / total
-
-#perform Canny edge detection using openCV
-edges = cv2.Canny(img_array, mean*0.75, mean*1.25)
+#perform Canny edge detection using the otsu threshold
+edges = cv2.Canny(img_array, otsu*0.5, otsu)
 
 #Display the edge detected image
 plt.imshow(edges, cmap='gray')
