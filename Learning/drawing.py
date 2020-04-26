1fe7a5ab35cf6b02c5851ac177a477740e91b141
ahmadtc1 <ahmadtc2000@gmail.com>
2020-04-26 16:12:01 -0400

    Learning to use opencv to draw on images.
    Learning line functions.
    Learning rectangle functions.
    Learning circle functions.
    
    Using these functions to make a drawing

---
+++ Learning/drawing.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+canvas = np.zeros((300,300, 3), dtype = "uint8")
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
+green = (0,255,0)
+red = (0,0,255)
+blue = (255,0,0)
+
+cv2.line(canvas, (0,0), (300,300), green)
+cv2.imshow("Line", canvas)
+cv2.waitKey(0)
+
+cv2.line(canvas, (300,0), (0, 300), red, 5)
+cv2.imshow("Line", canvas)
+cv2.waitKey(0)
+
+cv2.rectangle(canvas, (10,10), (60, 60), green, 3)
+cv2.imshow("Rectangle", canvas)
+cv2.waitKey(0)
+
+cv2.rectangle(canvas, (50, 200), (200, 225), red, 7)
+cv2.imshow("Rectangle", canvas)
+cv2.waitKey(0)
+
+cv2.rectangle(canvas, (200, 50), (225, 125), blue, -1)
+cv2.imshow("Rectangle", canvas)
+cv2.waitKey(0)
+
+
+
+canvas = np.zeros((300,300,3), dtype="uint8")
+
+(centerX, centerY) = (canvas.shape[1] // 2, canvas.shape[0] // 2)
+white = (0xff,0xff,0xff)
+for r in range(0, 175, 25):
+    cv2.circle(canvas, (centerX, centerY), r, white)
+
+cv2.imshow("Circles", canvas)
+cv2.waitKey(0)
+
+
+for i in range(25):
+    radius = np.random.randint(5, high=200)
+
+    color = np.random.randint(0, high=256, size=(3)).tolist()
+
+    point = np.random.randint(0, high = 300, size=(2))
+
+    cv2.circle(canvas, tuple(point), radius, color, -1)
+
+cv2.imshow("Randomized!", canvas)
+cv2.waitKey(0)
