4ee063521e32ec0508909823222ffa8ff3cf4f2a
ahmadtc1 <ahmadtc2000@gmail.com>
2020-07-16 17:21:13 -0400

    Learning image bitwise operations and colour splitting

---
+++ Learning/bitwise.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import argparse
+
+rectangle = np.zeros((300, 300), dtype="uint8")
+cv2.rectangle(rectangle, (25, 25), (275, 275), 255, -1)
+cv2.imshow("Rectangle", rectangle)
+
+circle = np.zeros((300, 300), dtype="uint8")
+cv2.circle(circle, (150, 150), 150, 255, -1)
+cv2.imshow("Circle", circle)
+
+bitwiseAnd = cv2.bitwise_and(rectangle, circle)
+cv2.imshow("bitwiseAnd", bitwiseAnd)
+
+bitwiseOr = cv2.bitwise_or(rectangle, circle)
+cv2.imshow("bitwiseOr", bitwiseOr)
+
+bitwiseXor = cv2.bitwise_xor(rectangle, circle)
+cv2.imshow("bitwiseXor", bitwiseXor)
+
+bitwiseNot = cv2.bitwise_not(circle)
+cv2.imshow("bitwiseNot", bitwiseNot)
+
+cv2.waitKey(0)
